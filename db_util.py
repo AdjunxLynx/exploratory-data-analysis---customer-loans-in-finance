7a3cb79ed955e7ce3aee17def630ac48af3944d5
Kamil <61058175+UnsoundPython25@users.noreply.github.com>
2023-10-12 14:14:09 +0100

    creates a sqlalchemy engine to connect to online database

--- db_util.py
+++ db_util.py
@@ -1,11 +1,21 @@
 import yaml
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_credentials():
+        with open('credentials.yaml', 'r') as file:
+            credentials = yaml.safe_load(file)
+        return credentials
 
 
 class RDSDatabaseConnector():
-    def init():
-        pass
+    def __init__(self, credentials):
+        url = ("postgresql+psycopg2" + "://" + str(credentials['RDS_USER']) + ":" + str(credentials['RDS_PASSWORD']) + "@" + str(credentials['RDS_HOST']) + "/" + str(credentials['RDS_DATABASE']))
+        print(url)
+        engine = create_engine(url)
 
-    def load_credentials():
-            with open('credentials.yaml', 'r') as file:
-                credentials = yaml.load(file)
-            return credentials
+
+    
+rds = RDSDatabaseConnector(load_credentials())
