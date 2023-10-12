d233f22eaebdd8a335eb73028ee894d2eb628ea0
Kamil <61058175+UnsoundPython25@users.noreply.github.com>
2023-10-12 13:28:47 +0100

    adds load_credentials fucntion. creates gitignore

--- db_util.py
+++ db_util.py
@@ -1,1 +1,11 @@
+import yaml
 
+
+class RDSDatabaseConnector():
+    def init():
+        pass
+
+    def load_credentials():
+            with open('credentials.yaml', 'r') as file:
+                credentials = yaml.load(file)
+            return credentials
