dbc8144472c6f15330889046563e3af8b24c96f5
Kamil <61058175+UnsoundPython25@users.noreply.github.com>
2023-10-12 14:51:45 +0100

    saves database to pandas dataframe. saves dataframe to local machine.

--- db_util.py
+++ db_util.py
@@ -1,7 +1,7 @@
 import yaml
-import psycopg2
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from pandasgui import show
 
 
 def load_credentials():
@@ -9,13 +9,20 @@ def load_credentials():
             credentials = yaml.safe_load(file)
         return credentials
 
+def save_pd_to_csv(df):
+     df.to_csv('loan_payments.csv')
 
 class RDSDatabaseConnector():
     def __init__(self, credentials):
-        url = ("postgresql+psycopg2" + "://" + str(credentials['RDS_USER']) + ":" + str(credentials['RDS_PASSWORD']) + "@" + str(credentials['RDS_HOST']) + "/" + str(credentials['RDS_DATABASE']))
-        print(url)
-        engine = create_engine(url)
+        url = ("postgresql+psycopg2://" + str(credentials['RDS_USER']) + ":" + str(credentials['RDS_PASSWORD']) + "@" + str(credentials['RDS_HOST']) + "/" + str(credentials['RDS_DATABASE']))
+        self.engine = create_engine(url)
 
+    def to_pandas_df(self):
+         with self.engine.connect() as conn:
+            db = conn.execute(text("SELECT * FROM loan_payments"))
+            df = pd.DataFrame(data = db)
+            return df
 
     
 rds = RDSDatabaseConnector(load_credentials())
+save_pd_to_csv(rds.to_pandas_df())
