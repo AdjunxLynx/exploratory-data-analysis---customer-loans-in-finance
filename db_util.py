2f27d63af128501fe246472ddc6eb4aa60971daf
Kamil <61058175+UnsoundPython25@users.noreply.github.com>
2023-10-12 15:06:52 +0100

    adds a function to view pandas df file seperately

--- db_util.py
+++ db_util.py
@@ -12,12 +12,17 @@ def load_credentials():
 def save_pd_to_csv(df):
      df.to_csv('loan_payments.csv')
 
+def load_csv_to_pd(filename):
+     df = pd.read_csv(filename)
+     show(df)
+
+
 class RDSDatabaseConnector():
     def __init__(self, credentials):
         url = ("postgresql+psycopg2://" + str(credentials['RDS_USER']) + ":" + str(credentials['RDS_PASSWORD']) + "@" + str(credentials['RDS_HOST']) + "/" + str(credentials['RDS_DATABASE']))
         self.engine = create_engine(url)
 
-    def to_pandas_df(self):
+    def db_to_pandas_df(self):
          with self.engine.connect() as conn:
             db = conn.execute(text("SELECT * FROM loan_payments"))
             df = pd.DataFrame(data = db)
@@ -25,4 +30,5 @@ class RDSDatabaseConnector():
 
     
 rds = RDSDatabaseConnector(load_credentials())
-save_pd_to_csv(rds.to_pandas_df())
+save_pd_to_csv(rds.db_to_pandas_df())
+load_csv_to_pd("loan_payments.csv")
