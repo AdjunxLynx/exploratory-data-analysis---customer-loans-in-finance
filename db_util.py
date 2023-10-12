0161d7065a8ca4eddf194fb7fa10232bb78ef7c5
Kamil <61058175+UnsoundPython25@users.noreply.github.com>
2023-10-12 15:17:47 +0100

    renamed variables to be descriptive, adds docstrings, adds main script execution logic

--- db_util.py
+++ db_util.py
@@ -1,20 +1,24 @@
-import yaml
-import pandas as pd
 from sqlalchemy import create_engine, text
 from pandasgui import show
+import yaml
+import pandas as pd
+
 
 
 def load_credentials():
-        with open('credentials.yaml', 'r') as file:
-            credentials = yaml.safe_load(file)
-        return credentials
+    """loads credentials from a file called 'credentials.yaml' and returns it as a dictionary"""
+    with open('credentials.yaml', 'r') as file:
+        credentials = yaml.safe_load(file)
+    return credentials
 
-def save_pd_to_csv(df):
-     df.to_csv('loan_payments.csv')
+def save_pd_to_csv(dataframe):
+    """takes input parameter and dumps into a file in directory called 'loan_payments.csv'"""
+    dataframe.to_csv('loan_payments.csv')
 
 def load_csv_to_pd(filename):
-     df = pd.read_csv(filename)
-     show(df)
+    """takes input filename as parameter and displays file as a large table"""
+    dataframe = pd.read_csv(filename)
+    show(dataframe)
 
 
 class RDSDatabaseConnector():
@@ -22,13 +26,17 @@ class RDSDatabaseConnector():
         url = ("postgresql+psycopg2://" + str(credentials['RDS_USER']) + ":" + str(credentials['RDS_PASSWORD']) + "@" + str(credentials['RDS_HOST']) + "/" + str(credentials['RDS_DATABASE']))
         self.engine = create_engine(url)
 
-    def db_to_pandas_df(self):
-         with self.engine.connect() as conn:
-            db = conn.execute(text("SELECT * FROM loan_payments"))
-            df = pd.DataFrame(data = db)
-            return df
+    def database_to_pandas_dataframe(self):
+        """uses SQLAlchemy engine to connect to online database, download all data in the table and to return as a pandas dataframe"""
+        with self.engine.connect() as conn:
+            database = conn.execute(text("SELECT * FROM loan_payments"))
+            dataframe = pd.DataFrame(data = database)
+            return dataframe
 
     
-rds = RDSDatabaseConnector(load_credentials())
-save_pd_to_csv(rds.db_to_pandas_df())
-load_csv_to_pd("loan_payments.csv")
+
+
+if __name__ == "__main__":
+    rdsdbc = RDSDatabaseConnector(load_credentials())
+    save_pd_to_csv(rdsdbc.database_to_pandas_dataframe())
+    load_csv_to_pd("loan_payments.csv")
