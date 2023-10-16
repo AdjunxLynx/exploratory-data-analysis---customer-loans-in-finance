8a6cbff89fe31de824f1658ae8cde0ef0c8a793a
Kamil <61058175+UnsoundPython25@users.noreply.github.com>
2023-10-16 20:00:52 +0100

    creates the fucntion to clean all the qualitative (non numerical) columns and data

--- db_util.py
+++ db_util.py
@@ -42,46 +42,73 @@ class DataTransform():
         """puts all the dataframe cleaning function into one callable function"""
 
         self.set_column_to_custom()
-        self.set_column_to_date()
-        #self.set_column_to_float()
-        #self.set_column_to_int()
-        self.set_numeric()
-        show(self.dataframe)
+        date_list = self.set_column_to_date() # to get the list of column names used to clean
+        qualitative_dataframe = self.set_numeric() # to get the list of column names not cleaned
+        self.set_qualitative(qualitative_dataframe, date_list)
 
-
-    def set_column_to_int(self):
-        """sets a list of column names to dtype integer in the dataframe given"""
-
-        #creates list of all column names to set to dtype int
-
-        
-        #creates a list containing "int32" with len = to set_list
-        int32_list = []
-        for i in range(len(set_list)):
-            int32_list.append("int32")
-        to_integer_list = dict(zip(set_list, int32_list))
-
-        self.dataframe.astype(to_integer_list)
-        
-
+        x = 'policy_code'
 
     def set_numeric(self):
-        numeric_list = ["id", "member_id", "loan_amount", "funded_amount", "funded_amount_inv", "term",  "int_rate", "employment_length", "annual_inc", "dti", "delinq_2yrs", "inq_last_6mths", "mths_since_last_delinq", "mths_since_last_delinq",  "open_accounts", "total_accounts", "out_prncp", "out_prncp_inv", "total_payment", "total_payment_inv", "total_rec_prncp", "total_rec_int", "total_rec_late_fee", "recoveries", "collection_recovery_fee", "last_payment_amount", "collections_12_mths_ex_med", "mths_since_last_major_derog"]
-        numeric_dataframe = self.dataframe[numeric_list]
-        qualitive_dataframe = self.dataframe.drop(columns = numeric_list)
-        for column in numeric_list:
-            self.dataframe[column] = pd.to_numeric(self.dataframe[column], errors = 'raise')
-        
+        numeric_list = ["id", "member_id", "loan_amount", "funded_amount", "funded_amount_inv", "term",  "int_rate", "instalment", "employment_length", "annual_inc", "dti", "delinq_2yrs", "inq_last_6mths", "mths_since_last_delinq", "mths_since_last_record",  "open_accounts", "total_accounts", "out_prncp", "out_prncp_inv", "total_payment", "total_payment_inv", "total_rec_prncp", "total_rec_int", "total_rec_late_fee", "recoveries", "collection_recovery_fee", "last_payment_amount", "collections_12_mths_ex_med", "mths_since_last_major_derog"]
 
+        qualitative_dataframe = self.dataframe.drop(columns = numeric_list)
+        for column in numeric_list:
+            self.dataframe[column] = pd.to_numeric(self.dataframe[column], errors = "raise")
+        return qualitative_dataframe
+    
+    def set_qualitative(self, qualitative_dataframe, date_list):
+            qualitative_dataframe = qualitative_dataframe.drop(columns = date_list)
+
+            A_to_G = ["A", "B", "C", "D", "E", "F", "G"]
+
+            sub_grade = [] 
+            for letter in A_to_G:
+                for number in range(1,6):
+                    sub_grade.append((letter + str(number)))
+
+            home_ownership_list = ["OWN", "RENT", "MORTGAGE", "OTHER"]
+            verification_status = ["Verified", "Source Verified", "Not Verified"]
+            payment_plan_list = ["y", "n"]
+            application_list = ["INDIVIDUAL"]
+            policy_code_list = [1]
+
+            loan_status_list = ["Fully Paid", "In Grace Period", "Charged Off", "Current", "Default", "Late (16-30 days)", "Late (31-120 days)", "Does not meet the credit policy. Status:Fully Paid", "Does not meet the credit policy. Status:Charged Off"]
+
+            purpose_list = ["car", "credit_card", "debt_consolidation", "educational", "home_improvement", "house", "major_purchase", "medical", "moving", "other", "renewable_energy", "small_business", "vacation", "wedding"]
+
+            grade_dtype = pd.CategoricalDtype(categories = A_to_G)
+            sub_grade_dtype = pd.CategoricalDtype(categories = sub_grade)
+            home_ownership_dtype = pd.CategoricalDtype(categories = home_ownership_list)
+            verification_dtype = pd.CategoricalDtype(categories = verification_status)
+            loan_status_dtype = pd.CategoricalDtype(categories = loan_status_list)
+            payment_plan_dtype = pd.CategoricalDtype(categories = payment_plan_list)
+            purpose_dtype = pd.CategoricalDtype(categories = purpose_list)
+            application_dtype = pd.CategoricalDtype(categories = application_list)
+            policy_dtype = pd.CategoricalDtype(categories = policy_code_list)
+
+
+            qualitative_dataframe["grade"] = qualitative_dataframe["grade"].astype(grade_dtype)
+            qualitative_dataframe["sub_grade"] = qualitative_dataframe["sub_grade"].astype(sub_grade_dtype)
+            qualitative_dataframe["home_ownership"] = qualitative_dataframe["home_ownership"].astype(home_ownership_dtype)
+            qualitative_dataframe["verification_status"] = qualitative_dataframe["verification_status"].astype(verification_dtype)
+            qualitative_dataframe["loan_status"] = qualitative_dataframe["loan_status"].astype(loan_status_dtype)
+            qualitative_dataframe["payment_plan"] = qualitative_dataframe["payment_plan"].astype(payment_plan_dtype)
+            qualitative_dataframe["purpose"] = qualitative_dataframe["purpose"].astype(purpose_dtype)
+            qualitative_dataframe["application_type"] = qualitative_dataframe["application_type"].astype(application_dtype)
+            qualitative_dataframe["policy_code"] = qualitative_dataframe["policy_code"].astype(policy_dtype)
+            
+
+            show(qualitative_dataframe)
 
 
     def set_column_to_date(self):
         """sets a list of column names to dtype datetime in the dataframe given"""
 
-        column_list = ["issue_date", "earliest_credit_line", "last_payment_date", "next_payment_date", "last_credit_pull_date"]
+        date_list = ["issue_date", "earliest_credit_line", "last_payment_date", "next_payment_date", "last_credit_pull_date"]
 
-        for column in column_list:
+        for column in date_list:
             self.dataframe[column] = pd.to_datetime(self.dataframe[column]).dt.date
+        return date_list
 
     def set_column_to_custom(self):
         """sets seperate columns to appropriate dtypes and cleans them using the dataframe given"""
