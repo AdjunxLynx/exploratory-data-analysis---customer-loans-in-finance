88caa920886962d7f0231d6f30685581ddf2f781
Kamil <61058175+UnsoundPython25@users.noreply.github.com>
2023-12-07 15:05:07 +0000

    adds function descriptions for all functions in DataFrameInfo class

--- db_util.py
+++ db_util.py
@@ -3,6 +3,7 @@ from pandasgui import show
 import yaml
 import pandas as pd
 import numpy as np
+import warnings
 
 
 def load_credentials():
@@ -132,33 +133,95 @@ class DataFrameInfo():
         pass
     
     def call_all_information(self, dataframe):
-        #self.describe_all_columns(dataframe)
-        self.get_null_percentage(dataframe)
-        self.get_statistics(dataframe)
+        """Calls all functions that analyses dataframe, and shows that as a smaller dataframe"""
+        self.describe_all_columns(dataframe)
+        statistical_df = self.get_statistics(dataframe)
         self.get_shape(dataframe)
+        self.get_mode(dataframe)
+        return statistical_df
 
     def describe_all_columns(self, dataframe):
+        """prints analysis on the dataframe, giving quick information on the dataframe"""
         print(dataframe.describe(percentiles = [.5]))
     
+    def get_distinct_categories(self, dataframe):
+        """Gets a list of all columns in the current dataframe with dtype of category
+        then counts the distinct values in these columns. 
+        returns a DataFrame with NaN values where the column dtype is not of category,
+        and then the distinct count when the column dtype is"""
+        headers = self.get_column_headers(dataframe)
+        categorical_columns = dataframe.select_dtypes(include=['category']).columns.tolist()
+        distinct_values_count = {}
+
+        for column in dataframe.columns:
+            if column in categorical_columns:
+                distinct_values_count[column] = dataframe[column].nunique()
+            else:
+                distinct_values_count[column] = np.nan
+
+        distinct_values_df = pd.DataFrame.from_dict(distinct_values_count, orient='index', columns=["Distinct_Values_Count"])
+        distinct_values_df.index.name = 'Column_Name'
+
+
+        return distinct_values_df
+
     def get_statistics(self, dataframe):
+        """returns a Dataframe from the inputted Dataframe, giving statistical data on the Dataframe such as
+        non-null count, mean value of all the data in a column, minimum value, maximum value, standard deviation of the column
+        median value of column, percentage of value that is Null (np.NaN), amount of distinct values in categorical columns, most frequent value"""
         count = dataframe.count()
         mean = dataframe.mean()
         min = dataframe.min()
         max = dataframe.max()
         std = dataframe.std()
-        dataframes = [count, mean, min, max, std]
+        med = dataframe.median()
+        mode = self.get_mode(dataframe)
+        null_percentage_df = self.get_null_percentage(dataframe)
+        distinct_df = self.get_distinct_categories(dataframe)
+
+        dataframes = [count, mean, min, max, std, med, mode, null_percentage_df, distinct_df]
         statistical_df = pd.concat(dataframes, axis = 1)
-        statistical_df = statistical_df.rename(columns={0: "Count", 1: "Mean", 2: "Min", 3: "Max", 4: "Standard Deviation"}, errors = "raise")
-        show(statistical_df)
+        statistical_df = statistical_df.rename(columns={0: "Count", 1: "Mean", 2: "Min", 3: "Max", 4: "Standard Deviation", 5: "Median"}, errors = "raise").T
         return statistical_df
+    
+    def get_mode(self, dataframe):
+        """returns the most frequent value for each column in the given dataframe"""
+        mode_series = dataframe.mode().iloc[0]
+        mode_df = pd.DataFrame({'Mode': mode_series})
+        return mode_df
 
     def get_null_percentage(self, dataframe):
-        pass
-    
-    def get_shape(self, dataframe):
-        print(dataframe.shape())
+        """returns the percentage of null values in the column"""
+        headers = self.get_column_headers(dataframe)
+        percentage_list = []
 
+        for (column_name, column_data) in dataframe.iteritems():
+            null_count = dataframe[column_name].isnull().sum()
+            total_count = len(dataframe[column_name]) 
+            percentage = ((null_count / total_count) * 100)
+            percentage_list.append(percentage)
+
+        null_percentage_series = pd.Series(percentage_list, index=headers)
+        null_percentage_df = pd.DataFrame(null_percentage_series, columns=['Null Percentages'])
+        return null_percentage_df
+
+    def get_column_headers(self, dataframe):
+        """returns a list of the column titles from the dataframe given"""
+        column_names = []
+        for (column_name, column_data) in dataframe.iteritems():
+            column_names.append(column_name)
+
+        return column_names
+
+    def get_shape(self, dataframe):
+        """prints the shape (dimension) of the dataframe given"""
+        print("The Dataframe Shape is: " , dataframe.shape)
+
+   
 if __name__ == "__main__":
+
+    warnings.simplefilter(action='ignore', category=FutureWarning) # to stop spam of deprecated feature
+
     rdsdbc = RDSDatabaseConnector(load_credentials())
     save_pd_to_csv(rdsdbc.database_to_pandas_dataframe())
     dataframe = load_csv_to_pd("loan_payments.csv")
@@ -167,6 +230,6 @@ if __name__ == "__main__":
     dataframe = dt.call_all_cleaners(dataframe)
 
     dti = DataFrameInfo(dataframe)
-    dti.call_all_information(dataframe)
+    statistical_df = dti.call_all_information(dataframe)
 
-    #show(dataframe)
+    show(statistical_df)
