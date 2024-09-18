941009b24a73f6675a17816196316f8c3dc88771
Kamil <kleocadieolsen@gmail.com>
2024-09-18 14:07:03 +0100

    Places all modules in 'Library' file. Refactors code to work around Analysis.ipynb file. Makes running the notebook more visually appealing and informative

---
+++ Library/DTransform.py
@@ -0,0 +1,146 @@
+import pandas as pd
+
+class DataTransform():
+    def __init__(self):
+        self.available_list = []
+        self.string_list = []
+        self.strip_list = []
+        self.qualitative_list = []
+    
+    def call_all_cleaners(self, dataframe):
+        """puts all the dataframe cleaning function into one callable function"""
+        
+        numeric_list = self.get_numeric_list()
+        date_list = self.get_date_list()
+        
+        dataframe = self.set_column_to_custom(dataframe)
+        dataframe = self.set_column_to_date(dataframe, date_list)
+
+        dataframe = self.set_numeric(dataframe, numeric_list)
+        dataframe = self.set_qualitative(dataframe,self.qualitative_list)
+        dataframe = dataframe.rename(columns = {"Unnamed: 0": "Index"})
+
+        return(dataframe)
+    
+    def drop_columns_in_series(self, series, columns):
+        """Drops all indexs where the index name is in columns list to drop"""
+        
+        for column in columns:
+            try:
+                series = series.drop(column)
+            except:
+                pass
+        return series
+    
+    def get_skewed_columns(self, dataframe, qualitative_list, threshold=0.5):
+        """Inputs the current dataframe and returns all the columns where the columns are skewed by more than 0.5 (moderately skewed), considering only numeric columns."""
+        
+        numeric_cols = dataframe.select_dtypes(include=['number'])
+        skewed_columns = numeric_cols.skew().where(lambda x: abs(x) > threshold).dropna()
+        skewed_columns = skewed_columns.drop(labels=qualitative_list, errors='ignore')
+        skewed_columns = skewed_columns.index.tolist()
+        return skewed_columns
+    
+    def get_skewed_dataframe(self, dataframe, skewed_columns):
+        """returns the sub frame of dataframe that only includes the columns inputted"""
+        
+        skewed_dataframe = dataframe[skewed_columns]
+        return skewed_dataframe
+        
+    
+    def get_numeric_list(self):
+        """returns a list of the column names that are numeric"""
+        
+        numeric_list = ["id", "member_id", "loan_amount", "funded_amount", "funded_amount_inv", "term",  "int_rate", "instalment", "employment_length", "annual_inc", "dti", "delinq_2yrs", "inq_last_6mths", "mths_since_last_delinq",  "open_accounts", "total_accounts", "out_prncp", "out_prncp_inv", "total_payment", "total_payment_inv", "total_rec_prncp", "total_rec_int", "total_rec_late_fee", "recoveries", "collection_recovery_fee", "last_payment_amount", "collections_12_mths_ex_med", "mths_since_last_major_derog"]
+        return numeric_list
+    
+    def get_date_list(self):
+        """returns a list of the column names that are dates"""
+        
+        date_list = ["issue_date", "earliest_credit_line", "last_payment_date", "next_payment_date", "last_credit_pull_date"]
+        return date_list
+
+    def set_numeric(self, dataframe, numeric_list):
+        """turns all columns listed in numeric_list in the dataframe given to a numeric dtype"""
+        
+        for column in numeric_list:
+            
+            dataframe[column] = pd.to_numeric(dataframe[column], errors = "coerce", downcast = "integer")
+            dataframe[column] = dataframe[column].round()
+            dataframe[column] = dataframe[column].apply(lambda x: round(x) if not pd.isnull(x) else x).astype("Int64")
+
+        return(dataframe)
+
+    def set_column_to_date(self, dataframe, date_list):
+        """sets a list of column names to dtype datetime in the dataframe given"""
+        for column in date_list:
+            dataframe[column] = pd.to_datetime(dataframe[column], errors = "coerce", format='%b-%Y').dt.date
+        return(dataframe)
+
+    def set_column_to_custom(self, dataframe):
+        """sets seperate columns to appropriate dtypes and cleans them using the dataframe given"""
+        
+        dataframe = self.set_column_to_string(dataframe, self.string_list)
+        dataframe = self.set_column_to_available_or_not(dataframe, self.available_list)
+        dataframe = self.strip_column_to_int(dataframe, self.strip_list)
+        dataframe = self.set_qualitative(dataframe, self.qualitative_list)
+
+        return(dataframe)
+    
+    def strip_column_to_int(self, dataframe, columns):
+        """sets all the columns thats match the given columns list, and strips of all characters except integers"""
+        
+        for column in columns:
+            dataframe[column] = dataframe[column].str.replace(r"\D", "", regex=True)
+        return dataframe
+             
+    
+    def set_qualitative(self, dataframe, categorical_list):
+        """creates new catagorical dtypes of each qualitative column, and sets that column as the new dtype to save space.
+        some dtypes are also ordered"""
+        
+        for column in categorical_list:
+            dataframe[column] = pd.Categorical(dataframe[column])
+            
+        return(dataframe)
+    
+    
+    
+    
+    def set_column_to_string(self, dataframe, columns):
+        """sets all the columns thats match the given columns list to string"""
+        
+        for column in columns:
+            dataframe[column] = dataframe[column].astype(str)
+            dataframe[column] = dataframe[column].str.replace(r"\D", "", regex=True)
+            
+        return dataframe
+    
+    def set_column_to_available_or_not(self, dataframe, columns):
+        """sets all the columns thats match the given columns list to Available or Not Available"""
+        
+        for column in columns:
+            dataframe[column] = dataframe[column].str.replace("1", "Available")
+            dataframe[column] = dataframe[column].str.replace("0", "Not Available")
+            dataframe[column] = dataframe[column].astype(str)
+        return dataframe
+    
+    def set_available_list(self, list):
+        """setter method for class list available_list"""
+        
+        self.available_list = list
+        
+    def set_qualitative_list(self, list):
+        """setter method for class list available_list"""
+        
+        self.qualitative_list = list
+        
+    def set_string_list(self, list):
+        """setter method for class list available_list"""
+        
+        self.string_list = list
+    
+    def set_strip_list(self, list):
+        """setter method for class list available_list"""
+        
+        self.strip_list = list
---
+++ Library/GraphPlotter.py
@@ -0,0 +1,172 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from matplotlib.ticker import ScalarFormatter, FuncFormatter
+
+
+
+class Plotter:
+    def plot_nulls_before_after(self, before, after):
+        """creates two barcharts of the columns in the dataframe, before and after they have been cleaned of all null values. shows them side by side"""
+        
+        fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+        plt.tight_layout()
+        plt.subplots_adjust(bottom=0.4)
+        before.plot.bar(ax=ax[0], title="Null Values Before")
+        after.plot.bar(ax=ax[1], title="Null Values After")
+        plt.show()
+        plt.close(fig)
+        
+    def plot_skewed_vs_unskewed_graph(self, skewed_dataframe, unskewed_dataframe):
+        """creates two barcharts of the skewness of columns in the dataframe, before and after they have been transformed. shows them side by side"""
+
+        fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+        plt.tight_layout()
+        plt.subplots_adjust(bottom=0.4)
+        skewed_dataframe.plot.bar(ax=ax[0], title="Skewness Before")
+        unskewed_dataframe.plot.bar(ax=ax[1], title="Skewness After")
+        plt.show()
+        plt.close(fig)
+        
+    def plot_outliers_before_after(self, before, after):
+        """creates two barcharts of the columns in the dataframe, before and after they have been cleaned of all outlier values. shows them side by side"""
+        
+        
+        fig, ax = plt.subplots(1, 2, figsize=(12, 6))
+        plt.tight_layout()
+        plt.subplots_adjust(bottom=0.4)
+        before.plot.bar(ax=ax[0], title="Amount of outliers before")
+        after.plot.bar(ax=ax[1], title="Amount of outliers after")
+        plt.show()
+        plt.close(fig)
+        
+    def visualise_outliers(self, dataframe, outlier_columns, show = True):
+        """creates a boxplot of each column in input to visualise outliers in the dataset"""
+        
+        #columns where I deemed the outliers to be irrelevent to analysing and viewing the dataset as a whole.
+        
+        # funded_amount/inv because there are so little, and too far from median.
+        # open/total_accounts because it is odd to have 0 accounts open.
+        # outliers above 5 open accounts seem irrelevent to me as there are so little of them.
+        # delinq because they are so far from the only value.
+        # total_payment/inv because the values are too from the median.
+        # last_payment_amount as the values are too scattered bellow the lower bounds.
+        
+        
+        
+        
+        for column in dataframe.columns:
+            if column in outlier_columns:
+                if show:
+                    plt.figure()  # new figure for each plot
+                    sns.boxplot(data=dataframe[column])
+                    plt.title(f"Box Plot of {column}")
+                    plt.show()
+                    plt.close()
+            
+    def plot_correlation(self, matrix):
+        """creates a heatmap given a correlation matrix to help visualise correlations between columns"""
+        
+        plt.figure(figsize=(12, 8))
+        plt.title(f"HeatMap for correlation")
+        sns.heatmap(matrix, annot=True, fmt=".2f", cmap='coolwarm', annot_kws={"size": 7})
+        plt.show()
+        plt.close()
+                
+    def plot_correlation_before_after(self, before_matrix, after_matrix):
+        """creates two heatmaps, one visualising the dataframe before, and after removing columns"""
+        
+        # Create a figure with 2 subplots side by side
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(30, 8))
+
+        sns.heatmap(before_matrix, ax=ax1, cmap="coolwarm", annot=True)
+        ax1.set_title('Correlation heatmap for Before')
+        sns.heatmap(after_matrix, ax=ax2, cmap="coolwarm", annot=True)
+        ax2.set_title('Correlation heatmap for After')
+
+        plt.show()
+        plt.close(fig)
+        
+    def plot_recovery_rate(self, recovery_percentage):
+        plt.figure(figsize=(10, 6))
+        plt.hist(recovery_percentage, bins=10, color='blue', edgecolor='black')
+        plt.xlabel('Recovery Percentage (%)')
+        plt.ylabel('Number of Loans')
+        plt.title('Distribution of Loan Recovery Percentages')
+        plt.show()
+        
+    def plot_loan_to_pay(self, loans_percentage_to_pay):
+        plt.figure(figsize=(10, 6))
+        plt.hist(loans_percentage_to_pay, bins=10, color='blue', edgecolor='black')
+        plt.xlabel('Percentage left to pay (%)')
+        plt.ylabel('Number of Loans')
+        plt.title('Distribution of Loan left to pay Percentages')
+        plt.show()
+        
+    def plot_amount_payed_6mths(self, amount_payed_6mths):
+        plt.figure(figsize=(10, 6))
+        plt.hist(amount_payed_6mths, bins=10, color='blue', edgecolor='black')
+        plt.xlabel('Projected total payment for 6 months (£)')
+        plt.ylabel('Number of Loans')
+        plt.title('Distribution of projected total payments for 6 months')
+        plt.show()
+        
+    
+        
+    def plot_loss_from_charged_off(self, loss_by_bucket):
+        def format_y_axis(value, tick_number):
+            return f"{int(value):,}"
+    
+        plt.figure(figsize = (10,6))
+        loss_by_bucket.plot(kind = "bar", color = "red")
+        
+
+        plt.gca().yaxis.set_major_formatter(ScalarFormatter(useOffset=False))
+        plt.gca().yaxis.set_major_formatter(FuncFormatter(format_y_axis))
+        
+        
+        plt.xlabel("Remaining Term (Months)")
+        plt.ylabel("Total expected loss (£)")
+        plt.title("Expected loss aggregated by remaining term")
+        plt.show()
+        
+    def visualise_loan_grade(self, dataframe):
+        plt.figure(figsize=(10, 6))
+        sns.countplot(data=dataframe, x="grade", hue="loan_status")
+        plt.title("Distribution of Loan Sub Grades by Loan Status")
+        plt.xlabel("Loan Grade")
+        plt.ylabel("Number of Loans")
+        plt.show()
+        
+    def visualise_loan_subgrade(self, dataframe):
+        plt.figure(figsize=(10, 6))
+        dataframe = dataframe.sort_values(by="sub_grade")
+        sns.countplot(data=dataframe, x="sub_grade", hue="loan_status")
+        plt.title("Distribution of Loan Grades by Loan Status")
+        plt.xlabel("Loan Grade")
+        plt.ylabel("Number of Loans")
+        plt.show()
+        
+    def visualise_loan_purpose(self, dataframe):
+        plt.figure(figsize=(12, 8))
+        sns.countplot(data=dataframe, y="purpose", hue="loan_status", order=dataframe["purpose"].value_counts().index)
+        plt.title("Distribution of Loan Purposes by Loan Status")
+        plt.xlabel("Number of Loans")
+        plt.ylabel("Loan Purpose")
+        plt.show()
+        
+    def visualise_loan_home(self, dataframe):
+        plt.figure(figsize=(10, 6))
+        sns.countplot(data=dataframe, x="home_ownership", hue="loan_status")
+        plt.title("Distribution of Home Ownership by Loan Status")
+        plt.xlabel("Home Ownership Status")
+        plt.ylabel("Number of Loans")
+        plt.show()
+        
+    def visualise_loan_term(self, dataframe):
+        plt.figure(figsize=(10, 6))
+        sns.countplot(data=dataframe, x="term", hue="loan_status")
+        plt.title("Distribution of Loan Terms by Loan Status")
+        plt.xlabel("Term Length")
+        plt.ylabel("Number of Loans")
+        plt.show()
